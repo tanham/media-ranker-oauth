b07c1d383ee692678b5134989d52c3812ee24d9a
Tanisha <tanishahampden@gmail.com>
2017-10-28 17:05:25 -0700

    add method to create a new user from auth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+  def self.from_auth_hash(provider, auth_hash)
+    user = new
+    user.provider = provider
+    user.uid = auth_hash['uid']
+    user.username = auth_hash['info']['name'] || auth_hash['info']['nickname']
+    user.email = auth_hash['info']['email']
+
+    return user
+  end
+
 end
