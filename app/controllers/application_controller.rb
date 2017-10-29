4e97e3cc171648ad068eb71ccdb859d8ef3758b3
Tanisha <tanishahampden@gmail.com>
2017-10-29 13:18:53 -0700

    add require_login methd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
+  before_action :find_user, :require_login
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
@@ -9,9 +9,22 @@ class ApplicationController < ActionController::Base
   end
 
 private
+
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
     end
   end
+
+protected
+
+def require_login
+  @login_user = User.find_by(id: session[:user_id])
+  unless @login_user
+    flash[:status] = :failure
+    flash[:result_text] = "You must be logged in to do that!"
+    redirect_to root_path
+  end
+end
+
 end
